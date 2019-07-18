d0ace07c3637a7d022533f4138758e8fdb466270
denis-axxis <denis.perez@axxis-systems.com>
2019-07-18 18:09:05 -0500

    Prácticas con triángulos 2

--- 2-2-Triangles-1/Main.cpp
+++ 2-2-Triangles-1/Main.cpp
@@ -1,4 +1,4 @@
-#include <framework.h>
+﻿#include <framework.h>
 
 using namespace Glut;
 using namespace OpenGL;
@@ -8,7 +8,7 @@ void Display();
 
 int WINAPI wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ INT32)
 {
-    constexpr int32_t width = 800, height = 500;
+    constexpr int32_t width = 500, height = 500;
     const auto desktop_size = GlutDesktopSize();
     const auto [x, y] = CenteredCoordinates(desktop_size, width, height);
 
@@ -25,28 +25,35 @@ int WINAPI wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ INT32)
 }
 
 void DrawTriangles();
+void DrawTrianglesStrip();
+void DrawTrianglesFan();
+void DrawTrianglesFan2();
+void DrawTrianglesFan3();
 
 void Display()
 {
-    ClearColor::White();
+    ClearColor::Rgb(0x393276);
     Clear(ClearMask::ColorBufferBit);
     SetMatrixMode(MatrixMode::Projection);
     LoadIdentity();
     glOrtho(Left(-100), Right(100), Bottom(-100), Top(100), Near(-100), Far(100));
     SetMatrixMode(MatrixMode::ModelView);
-    DrawTriangles();
-
+    // DrawTriangles();
+    // DrawTrianglesStrip();
+    // DrawTrianglesFan();
+    // DrawTrianglesFan2();
+    DrawTrianglesFan3();
     Flush();
 }
 
-void DrawTriangles()
+inline void DrawTriangles()
 {
+    // https://iubatians.blogspot.com/2015/06/c-opengl-10-draw-triangle.html
+    // Vertices anti-horario manualmente
+
     Begin(BeginMode::Triangles);
     {
-        // glColor3d(0.9373, 0.7216, 0.0627);
-        // HexColor3d(0xEF, 0xB8, 0x10);
-        // ArgbColor(0xFF'EF'B8'10);
-        RgbColor(0xEFB810);
+        RgbColor(0xB40500);
 
         // Triangulo 1
         glVertex2d(X(-99), Y(-99));
@@ -62,6 +69,103 @@ void DrawTriangles()
         glVertex2d(X(-49.5), Y(0));
         glVertex2d(X(49.5), Y(0));
         glVertex2d(X(0), Y(99));
+    }
+    End();
+}
+
+inline void DrawTrianglesStrip()
+{
+    // https://iubatians.blogspot.com/2015/06/c-opengl-11-draw-triangle-strip.html
+    // Vertices anti-horario automáticamene
+
+    Begin(BeginMode::TriangleStrip);
+    {
+        RgbColor(0xFF0000);
+        glVertex2d(X(0), Y(0));
+        glVertex2d(X(-99), Y(33));
+        glVertex2d(X(-99), Y(-33));
+        RgbColor(0x00FF00);
+        glVertex2d(X(0), Y(0));
+        glVertex2d(X(-33), Y(-99));
+        RgbColor(0x0000FF);
+        glVertex2d(X(33), Y(-99));
+        glVertex2d(X(0), Y(0));
+        RgbColor(0xF0F0F0F);
+        glVertex2d(X(99), Y(-33));
+        RgbColor(0xFFFFFF);
+        glVertex2d(X(99), Y(33));
+        glVertex2d(X(0), Y(0));
+        RgbColor(0xFFFF00);
+        glVertex2d(X(33), Y(99));
+        RgbColor(0x00FFFF);
+        glVertex2d(X(-33), Y(99));
+        RgbColor(0x0F0F0F);
+        glVertex2d(X(0), Y(0));
+        glVertex2d(X(-99), Y(33));
+    }
+    End();
+}
+
+inline void DrawTrianglesFan()
+{
+    Begin(BeginMode::TriangleFan);
+    {
+        RgbColor(0xEF1097);
+        glVertex2d(0, 0);
+        glVertex2d(-50, 50);
+        glVertex2d(-50, -50);
+        glVertex2d(50, -50);
+        glVertex2d(50, 50);
+    }
+    End();
+
+    Begin(BeginMode::TriangleFan);
+    {
+        RgbColor(0xFFF00C);
+        glVertex2d(0, 10);
+        glVertex2d(-40, 50);
+        glVertex2d(40, 50);
+    }
+    End();
+}
+
+inline void DrawTrianglesFan2()
+{
+    // Dibujando TriangleFan
+    // https://stackoverflow.com/questions/8043923/gl-triangle-fan-explanation
+    // https://iubatians.blogspot.com/2015/06/c-opengl-12-draw-triangle-fan.html
+
+    // Sector circular
+    // https://www.matesfacil.com/ESO/geometria_plana/circular/sector/sector-circular-circunferencia-radio-area-perimetro-problemas-resueltos.html
+
+    // Vertices horario automáticamene
+    Begin(BeginMode::TriangleFan);
+    {
+        RgbColor(0xCFE01F);
+        glVertex3d(X(0.0), Y(0.0), Z(75.0));
+
+        // Si el ángulo es α = 2π radianes(ó 360 grados), el sector circular es un círculo completo.
+        for (GLdouble angle = 0.0; angle < (2.0 * PI); angle += 0.5)
+        {
+            GLdouble x = /*Radio*/50.0 * sin(angle);
+            GLdouble y = /*Radio*/50.0 * cos(angle);
+            glVertex2d(x, y);
+        }
+    }
+    End();
+}
+
+inline void DrawTrianglesFan3()
+{
+    Begin(BeginMode::TriangleFan);
+    {
+        RgbColor(0xFF0000);
+    }
+    End();
+
+    Begin(BeginMode::TriangleFan);
+    {
+        RgbColor(0x000000);
     }
     End();
 }
